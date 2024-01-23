b6ae14d6f8c10bfbd7a39c2e6c7b35d6432b09ef
jaranchai <jaranchai.nt@hotmail.com>
2024-01-23 20:27:06 +0700

    [Edited] remove code

--- src/app/cart/view.tsx
+++ src/app/cart/view.tsx
@@ -6,9 +6,6 @@ const CartView = () => {
   return (
     <div className='bg-white'>
       <div className='pt-6'>Cart</div>
-      <div className='mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16'>
-        sss
-      </div>
     </div>
   )
 }
